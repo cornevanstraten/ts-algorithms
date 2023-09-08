ac40b7c384f4718a4af6e9d87422c2c1b680fe80
Corné van Straten <corne.straten@array.com>
2023-09-08 14:36:46 -0400

    walking a binary tree in preorder, inorder, and postorder

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,22 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+/* IN ORDER: go left, visit the node, then go right. */
 
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    //base case
+    if (!curr) {
+        return path;
+    }
+
+    //pre
+
+    //recurse
+    walk(curr.left, path);
+    path.push(curr.value);
+    walk(curr.right, path);
+
+    //post
+    return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,22 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+/* POST ORDER: go left, then go right, then visit the node. */
 
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    //base case
+    if (!curr) {
+        return path;
+    }
+
+    //pre
+
+    //recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    //post
+    path.push(curr.value);
+    return path;
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
